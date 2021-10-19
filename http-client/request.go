6eb03bb5e6f2d6193bcab1a544ea47943f31a43c
zhangshiyang <791978082@qq.com>
2021-10-19 15:36:55 +0800

    feature: http client add tls

--- http-client/request.go
+++ http-client/request.go
@@ -2,25 +2,48 @@ package client
 
 import (
 	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"reflect"
+	"time"
 )
 
 type PreHandler interface {
 	PreHandler(body []byte) ([]byte, error)
 }
 
+type Response struct {
+	Code int
+	Body []byte
+}
+
+func (s Response) String() string {
+	resp, err := json.Marshal(s)
+	if err != nil {
+		return ""
+	}
+
+	return string(resp)
+}
+
+func (s Response) Error() string {
+	return s.String()
+}
+
 type request struct {
+	tls         *tls.Config
 	request     *http.Request
 	preHandlers PreHandler
+	timeout     time.Duration
 }
 
 func (c *client) Post(body []byte) *request {
 	buffer := bytes.NewBuffer(body)
 	url := fmt.Sprintf("%s?%s", c.url, c.urlValues.Encode())
 	req, _ := http.NewRequest("POST", url, buffer)
+
 	req.Header = c.headers.Clone()
 	return &request{request: req}
 }
@@ -47,19 +70,33 @@ func (c *client) Put(body []byte) *request {
 	return &request{request: req}
 }
 
+func (r *request) TLS(tls *tls.Config) *request {
+	r.tls = tls
+	return r
+}
+
 func (r *request) AddPreHandler(handler PreHandler) *request {
 	r.preHandlers = handler
 	return r
 }
+func (r *request) SetTimeout(sec time.Duration) *request {
+	r.timeout = sec * time.Second
+	return r
+}
 
 func (r *request) Do(model interface{}) (err error) {
-	// check wether
+	// check weather
 	rv := reflect.ValueOf(model)
 	if rv.Kind() != reflect.Ptr || rv.IsNil() {
 		return &json.InvalidUnmarshalError{Type: reflect.TypeOf(model)}
 	}
 
-	c := http.Client{}
+	c := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: r.tls,
+		},
+		Timeout: r.timeout,
+	}
 	response, err := c.Do(r.request)
 	if err != nil {
 		return err
@@ -69,9 +106,15 @@ func (r *request) Do(model interface{}) (err error) {
 	}()
 
 	var buf bytes.Buffer
-	_, _ = buf.ReadFrom(response.Body)
+	_, err = buf.ReadFrom(response.Body)
+	if err != nil {
+		return err
+	}
 	if response.StatusCode >= 400 {
-		return fmt.Errorf(buf.String())
+		return Response{
+			Code: response.StatusCode,
+			Body: buf.Bytes(),
+		}
 	}
 
 	//自定义数据预处理方法
